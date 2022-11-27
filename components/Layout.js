0a2c3257399313d1c4c3036146f8a038f2805f97
hassan <hassanfathy593@gmail.com>
2022-11-27 14:37:19 +0200

    deleted layout nav temporarily

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,12 @@
 function Layout({children}) {
     return ( 
         <>
-        <nav className="mb-5 flex flex-row gap-2 justify-between items-center">
+        {/* <nav className="mb-5 flex flex-row gap-2 justify-between items-center">
            <div>Components</div>
            <div>this is nav</div>
            <div>this is nav</div>
-        </nav>
-        <main className="flex flex-col gap-2 items-center">{children}</main>
+        </nav> */}
+        <main className="p-4 flex flex-col gap-2 items-center">{children}</main>
         </>
      );
 }
